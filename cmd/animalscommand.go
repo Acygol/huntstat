7f32bcb775d1109af3ff7f901b974b1483d39203
Acygol <35696477+Acygol@users.noreply.github.com>
2018-12-23 05:14:07 +0100

    Updated animal list

--- cmd/animalscommand.go
+++ cmd/animalscommand.go
@@ -6,20 +6,23 @@ import (
 	"time"
 )
 
+// Also used in leaderboardcommand.go
+var ANIMALS = [...]string {
+	"Alpine Ibex", "American Black Duck", "Arctic Fox", "Banteng",
+	"Bighorn Sheep", "Bison", "Black Bear", "Blacktail Deer",
+	"Bobcat", "Brown Bear", "Canada Goose", "Cottontail Rabbit",
+	"Coyote", "Dallsheep", "Eurasian Lynx", "European Rabbit",
+	"Feral Goat", "Feral Hog", "Gadwall", "Grey Wolf",
+	"Grizzly Bear", "Magpie Goose", "Mallard", "Moose",
+	"Mule Deer", "Northern Pintail", "Pheasant", "Polar Bear",
+	"Red Deer", "Red Fox", "Red Kangaroo", "Reindeer",
+	"Rock Ptarmigan", "Rocky Mountain Elk", "Roe Deer", "Roosevelt Elk",
+	"Rusa deer", "Sambar Deer", "Sitka Deer", "Snowshoe Hare",
+	"Turkey", "Water Buffalo", "White-tailed Ptarmigan", "Whitetail",
+	"Wild Boar", "Willow Ptarmigan",
+}
+
 func AnimalsCommand(ctx framework.Context) {
 	rand.Seed(time.Now().UnixNano())
-	animals := []string {
-		"Alpine Ibex", "Arctic Fox", "Banteng", "Bighorn Sheep",
-		"Bison", "Black Bear", "Blacktail Deer", "Bobcat",
-		"Brown Bear", "Canada Goose", "Cottontail Rabbit", "Coyote",
-		"Dall Sheep", "Eurasian Lynx", "European Rabbit", "Feral Goat",
-		"Feral Hog", "Grey Wolf", "Grizzly Bear", "Magpie Goose",
-		"Duck", "Moose", "Mule Deer", "Pheasant",
-		"Ptarmigan", "Polar Bear", "Red Deer", "Red Fox",
-		"Red Kangaroo", "Reindeer", "Rocky Mountain Elk", "Roe Deer",
-		"Roosevelt Elk", "Rusa Deer", "Sambar Deer", "Sitka Deer",
-		"Snowshoe Hare", "Turkey", "Water Buffalo", "Whitetail Deer",
-		"Wild Boar",
-	}
-	ctx.Reply(animals[rand.Intn(len(animals))])
+	ctx.Reply(ANIMALS[rand.Intn(len(ANIMALS))])
 }
