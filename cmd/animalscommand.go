ffc0d3348522d8bc0d834ad4f9e143159f11fb4a
Acygol <35696477+Acygol@users.noreply.github.com>
2019-01-05 03:56:12 +0100

    Animal names are now stored in a JSON file

--- cmd/animalscommand.go
+++ cmd/animalscommand.go
@@ -1,28 +1,13 @@
 package cmd
 
 import (
-	"github.com/acygol/huntstat/framework"
 	"math/rand"
 	"time"
-)
 
-// Also used in leaderboardcommand.go
-var ANIMALS = [...]string {
-	"Alpine Ibex", "American Black Duck", "Arctic Fox", "Banteng",
-	"Bighorn Sheep", "Bison", "Black Bear", "Blacktail Deer",
-	"Bobcat", "Brown Bear", "Canada Goose", "Cottontail Rabbit",
-	"Coyote", "Dallsheep", "Eurasian Lynx", "European Rabbit",
-	"Feral Goat", "Feral Hog", "Gadwall", "Grey Wolf",
-	"Grizzly Bear", "Magpie Goose", "Mallard", "Moose",
-	"Mule Deer", "Northern Pintail", "Pheasant", "Polar Bear",
-	"Red Deer", "Red Fox", "Red Kangaroo", "Reindeer",
-	"Rock Ptarmigan", "Rocky Mountain Elk", "Roe Deer", "Roosevelt Elk",
-	"Rusa deer", "Sambar Deer", "Sitka Deer", "Snowshoe Hare",
-	"Turkey", "Water Buffalo", "White-tailed Ptarmigan", "Whitetail",
-	"Wild Boar", "Willow Ptarmigan",
-}
+	"github.com/acygol/huntstat/framework"
+)
 
 func AnimalsCommand(ctx framework.Context) {
 	rand.Seed(time.Now().UnixNano())
-	ctx.Reply(ANIMALS[rand.Intn(len(ANIMALS))])
+	ctx.Reply(framework.Animals[rand.Intn(len(framework.Animals))])
 }
---
+++ framework/animals.go
@@ -0,0 +1,43 @@
+package framework
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
+var Animals []string
+
+//
+// LoadAnimals reads in the JSON array containing
+// all theHunter animal names into Animals,
+// returning an error indicating success or failure
+//
+func LoadAnimals() error {
+	body, err := ioutil.ReadFile(filepath.FromSlash("data/animals.json"))
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &Animals)
+	return err
+}
+
+//
+// IsValidAnimalName takes as input an animal name
+// and checks if it is valid by sequentially
+// searching through Animals. It is sufficient to
+// check if the input name is a substring of an
+// actual animal name so that users don't have to
+// be extremely literal when using animal names
+// given that some are quite lengthy:
+//		e.g., "White-tailed Ptarmigan"
+//
+func IsValidAnimalName(name string) bool {
+	for _, n := range Animals {
+		if strings.Contains(strings.ToLower(n), strings.ToLower(name)) {
+			return true
+		}
+	}
+	return false
+}
